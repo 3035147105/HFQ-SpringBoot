742f3a6ea2c47e776f68983c9df5069fe0f85b56
3035147105@qq.com <3035147105@qq.com>
2018-05-30 14:53:34 +0800

    Receive增加ACK确认

--- src/main/java/com/toy/server/Receive.java
+++ src/main/java/com/toy/server/Receive.java
@@ -24,34 +24,38 @@ public class Receive {
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
         //
+        channel.basicQos(1);
 
         Consumer consumer = new DefaultConsumer(channel) {
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope,
                                        AMQP.BasicProperties properties, byte[] body)
                     throws IOException {
-                String message = new String(body, "UTF-8");
-                System.out.println(" [x] Received '" + message + "'");
 
+                String message = new String(body, "UTF-8");
                 try {
                     doWork(message);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } finally {
                     System.out.println(" [x] Done");
+                    channel.basicAck(envelope.getDeliveryTag(), false);
                 }
+
+                System.out.println(" [x] Received '" + message + "'");
+
             }
         };
 
         //
-        boolean autoAck = true;
+        boolean autoAck = false;
         channel.basicConsume(QUEUE_NAME, autoAck, consumer);
     }
 
     private static void doWork(String message) throws InterruptedException {
         for (char ch : message.toCharArray()) {
             if (ch == '.') {
-                Thread.sleep(1000);
+                Thread.sleep(3000);
             }
         }
     }
