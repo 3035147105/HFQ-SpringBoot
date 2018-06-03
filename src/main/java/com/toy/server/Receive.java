3be3f321b535c08b6dc561a69c0ad7ea608f76de
hfq-gaohaiqing <gaohaiqing@huizhaofang.com>
2018-06-03 23:34:21 +0800

    重写一遍Receive方法

--- src/main/java/com/toy/server/Receive.java
+++ src/main/java/com/toy/server/Receive.java
@@ -16,58 +16,40 @@ public class Receive {
 
     public static void main(String[] argv) {
 
-
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
-
         Connection connection = null;
+
         try {
-            connection      = factory.newConnection();
-            final Channel channel = connection.createChannel();
-
+            connection = factory.newConnection();
+            Channel channel = connection.createChannel();
             channel.queueDeclare(RPC_QUEUE_NAME, false, false, false, null);
-
             channel.basicQos(1);
-
-            System.out.println(" [x] Awaiting RPC requests");
+            System.out.println("Awaiting RPC Requests");
 
             Consumer consumer = new DefaultConsumer(channel) {
                 @Override
                 public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
-                    AMQP.BasicProperties replyProps = new AMQP.BasicProperties
-                            .Builder()
+                    AMQP.BasicProperties replyProps = new AMQP.BasicProperties.Builder()
                             .correlationId(properties.getCorrelationId())
                             .build();
 
-                    String response = "";
-
-                    try {
-                        String message = new String(body,"UTF-8");
-
-                        System.out.println(" [.] fib(" + message + ")");
-                        response = "server message";
-                    }
-                    catch (RuntimeException e){
-                        System.out.println(" [.] " + e.toString());
-                    }
-                    finally {
-                        channel.basicPublish( "", properties.getReplyTo(), replyProps, response.getBytes("UTF-8"));
-
-                        channel.basicAck(envelope.getDeliveryTag(), false);
-
-                        // RabbitMq consumer worker thread notifies the RPC server owner thread
-                        synchronized(this) {
-                            this.notify();
-                        }
+                    String requestMsg = new String(body, "UTF-8");
+                    System.out.println("get request msg:" + requestMsg);
+                    String response = "server response";
+                    channel.basicPublish("", properties.getReplyTo(), replyProps, response.getBytes("UTF-8"));
+                    channel.basicAck(envelope.getDeliveryTag(), false);
+                    // RabbitMq consumer worker thread notifies the RPC server owner thread
+                    synchronized (this) {
+                        this.notify();
                     }
                 }
             };
 
             channel.basicConsume(RPC_QUEUE_NAME, false, consumer);
 
-            // Wait and be prepared to consume the message from RPC client.
             while (true) {
-                synchronized(consumer) {
+                synchronized (consumer) {
                     try {
                         consumer.wait();
                     } catch (InterruptedException e) {
@@ -75,14 +57,21 @@ public class Receive {
                     }
                 }
             }
-        } catch (IOException | TimeoutException e) {
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
             e.printStackTrace();
         } finally {
-            if (connection != null)
+            if (connection != null) {
                 try {
                     connection.close();
-                } catch (IOException _ignore) {}
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+
     }
 
 }
